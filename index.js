26dbd7c8c4d6ba6f0a127ca4a942e8c17afa4daf
abdumurod9911 <khasanboyevabdumurod2@gmail.com>
2022-11-23 20:45:00 +0300

    fix

---
+++ index.js
@@ -0,0 +1,9 @@
+let nomer =+prompt('1-sonni kmiriting')
+for(let i = 1; i<= nomer; i++) {
+    if( i % 2 == 0){
+        console.log('juft son', i )
+    }
+    else {
+        console.log('toq son', i)
+    }
+}
